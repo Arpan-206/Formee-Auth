725c05fa992bf4cff742bd65a3170f4bd371a469
Arpan Pandey <arpanpandey.aps@gmail.com>
2022-03-19 10:59:47 +0530

    Added Signup

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from gql.transport.aiohttp import AIOHTTPTransport
 import jwt
 from typing import Optional
 from dotenv import load_dotenv
+from gql.transport.exceptions import TransportQueryError
 from fastapi import FastAPI, Header, HTTPException
 import os
 load_dotenv()
@@ -88,3 +89,69 @@ async def root(username: Optional[str] = Header(None), password: Optional[str] =
         return {"jwt": jwt.encode(user_jwt_data, jwt_key, algorithm=jwt_algorithm)}
 
     return HTTPException(status_code=401, detail="Invalid credentials")
+
+
+@app.get("/signup")
+async def signup(username: Optional[str] = Header(None), password: Optional[str] = Header(None), status_code: int = 200):
+    # Execute the query on the transport
+    if username is None or password is None:
+        raise HTTPException(
+            status_code=400, detail="Missing username or password")
+
+    jwt_key = os.getenv("JWT_KEY")
+    jwt_algorithm = os.getenv("JWT_ALGORITHM")
+    hasura_graphql_url = os.getenv("HASURA_GRAPHQL_ENDPOINT_URL")
+    if jwt_key is None:
+        raise HTTPException(
+            status_code=500, detail="Missing JWT key in .env")
+    if jwt_algorithm is None:
+        raise HTTPException(
+            status_code=500, detail="Missing JWT algorithm in .env")
+    if hasura_graphql_url is None:
+        raise HTTPException(
+            status_code=500, detail="Missing Hasura GraphQL URL in .env")
+
+    auth_jwt_data = {
+        "sub": "1234567890",
+        "name": "Auth",
+        "admin": False,
+        "iat": 1516239022,
+        "https://hasura.io/jwt/claims": {
+            "x-hasura-allowed-roles": ["user"],
+            "x-hasura-default-role": "user",
+            "x-hasura-user-id": "1234567890",
+        }
+    }
+
+    auth_encoded_jwt = jwt.encode(
+        auth_jwt_data, jwt_key, algorithm=jwt_algorithm)
+
+    # Select your transport with a defined url endpoint
+    transport = AIOHTTPTransport(url=hasura_graphql_url,
+                                 headers={"Authorization": "Bearer {}".format(auth_encoded_jwt)})
+
+    # Create a GraphQL client using the defined transport
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    # Provide a GraphQL query
+    auth_query = gql(
+        """
+    mutation CreateUser($password: String!, $username: String!) {
+  insert_cliWiki_User(objects: {username: $username, password: $password}) {
+    returning {
+      id
+      username
+      password
+      joined_on
+    }
+  }
+}
+
+    """
+    )
+    try:
+        result = await client.execute_async(auth_query, variable_values={"username": username, "password": password})
+    except TransportQueryError:
+        return HTTPException(status_code=400, detail="Username already exists")
+        
+    return result
